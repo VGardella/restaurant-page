904d0f9ccee1bc87c3be7d830bdc643a90a95eb0
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-02-20 21:59:26 -0300

    Add links to navegation bar

--- src/index.js
+++ src/index.js
@@ -8,9 +8,25 @@ const body = document.getElementById('container');
 
 const navMenu = document.createElement('nav');
 const board = document.createElement('div');
+const pages = document.createElement('ul');
+const home = document.createElement('li');
+const menu = document.createElement('li');
+const contact = document.createElement('li');
 
 navMenu.classList.add('nav-menu');
 board.classList.add('board');
+pages.classList.add('page-links');
+
+home.innerHTML = 'Home';
+menu.innerHTML = 'Menu';
+contact.innerHTML = 'Contact';
 
 body.appendChild(navMenu);
 body.appendChild(board);
+navMenu.appendChild(pages);
+pages.appendChild(home);
+pages.appendChild(menu);
+pages.appendChild(contact);
+
+const links = document.querySelectorAll('li');
+links.forEach((link) => link.classList.add('page-link'));
