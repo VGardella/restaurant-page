e358df46bb55625cc4f46f09aa48c4bbb3d999b1
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-02-20 21:43:13 -0300

    Create navegation bar and board

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+
+console.log('It\s working!')
+
+// Homepage
+
+const body = document.getElementById('container');
+
+const navMenu = document.createElement('nav');
+const board = document.createElement('div');
+
+navMenu.classList.add('nav-menu');
+board.classList.add('board');
+
+body.appendChild(navMenu);
+body.appendChild(board);
