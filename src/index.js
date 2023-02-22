8fe764501961c569c0090e401073b974e633f6a5
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-02-22 17:02:51 -0300

    Add Homepage functions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import './style.css';
 
 import { createNav } from './webpage';
 import { createBoard } from './webpage';
+import { insertLogo } from './home';
+import { insertText } from './home';
 
 createNav();
 createBoard();
+insertLogo();
+insertText();
