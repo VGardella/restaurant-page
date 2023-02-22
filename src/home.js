5751798585dde285381fc5afb354c4465eeedfce
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-02-22 17:02:13 -0300

    Divide text in two containers

--- src/home.js
+++ src/home.js
@@ -19,12 +19,17 @@ function insertLogo() {
 function insertText() {
     const board = document.querySelector('.board');
     const text = document.createElement('div');
+    const line1 = document.createElement('div');
+    const line2 = document.createElement('div');
 
-    text.innerHTML = 'Every flavor has a story';
+    line1.innerHTML = 'Serving the sweetest experiences since 2010.'
+    line2.innerHTML = 'Come explore a world full of flavors with us.';
 
     text.classList.add('subtext');
 
     board.appendChild(text);
+    text.appendChild(line1);
+    text.appendChild(line2);
 }
 
 export {insertLogo, insertText};
